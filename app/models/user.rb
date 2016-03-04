8791f54f788104ab5307535309ad4310c43cf62a
Iago <iago.moreira@codeminer42.com>
2016-03-03 22:41:30 -0300

    Add github integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  def self.create_from_omniauth(auth_hash)
+    self.create(provider: auth_hash[:provider],
+                uid: auth_hash[:uid],
+                name: auth_hash[:info][:name],
+                email: auth_hash[:info][:email],
+                avatar: auth_hash[:info][:image],
+                token: auth_hash[:credentials][:token],
+                nickname: auth_hash[:info][:nickname])
+  end
 end
