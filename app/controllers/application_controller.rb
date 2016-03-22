824836167f266d3e4d2c9c84310d752d01aacbca
Iago <iago.moreira@codeminer42.com>
2016-03-21 22:47:44 -0300

    Add dashboard#index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?
 
+  def authenticate_user
+    redirect_to root_path unless logged_in?
+  end
+
   def logged_in?
     current_user.present?
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def github_url(username)
+    "http://github.com/#{username}"
+  end
 end
